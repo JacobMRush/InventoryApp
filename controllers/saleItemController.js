ed70ef76bcb8bd8b518caa8900cd76edf5a4e660
JRush <jrjakerush@gmail.com>
2023-07-04 11:33:17 -0700

    working on controller for our Items

--- controllers/saleItemController.js
+++ controllers/saleItemController.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
 import Item from "../models/saleItem";
+
+//create item
+exports.manga_list = function(req,res,next) {
+    Item.find({item_title: {$ne: null}, item_publisher: {$ne: null}}).sort({item_publisher: 1}).exec((err, manga) => {
+        if(err) return next(err);
+        res.json(manga);
+    });
+}
+//update item
+//delete item
+//replace item
