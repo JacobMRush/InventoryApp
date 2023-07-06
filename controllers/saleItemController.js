0ee587b5ef2d74124b12a3ff281ebcccbe8b689c
JRush <jrjakerush@gmail.com>
2023-07-05 17:50:40 -0700

    working on saleItemController, layout for where each request will be sent from (url vs body params)

--- controllers/saleItemController.js
+++ controllers/saleItemController.js
@@ -12,20 +12,36 @@ exports.manga_list = function(req,res,next) {
 }
 //get a single item's details
 exports.manga_details = function(req,res,next) {
+    //by url
     Item.findOne()
 };
 //get items based on category
 exports.manga_category = function(req,res,next) {
-    
+    //by url - /category/${cat}
+    let category = req.params.category;
+    Item.find();
 }
 
 //create item
 exports.manga_create = function(req,res,next) {
-    Item.create();
+    //create a new item via request body details?
+    let citem = new Item({item_name, item_description, item_categories, price, number_in_stock, item_publisher, item_author});
+    
+    citem.save(function (err) {
+      if (err) {
+        cb(err, null);
+        return;
+      }
+      console.log("New Item: " + citem);
+      MangaItems.push(citem);
+      cb(null, citem);
+    });
 };
 //update item
 exports.manga_update = function(req,res,next) {
+    let mangaID = req.params._id;
     Item.findOneAndUpdate();
+
     //find item to update
     //change and update fields that are sent to us
     //create field/object if it does not already exist
@@ -33,12 +49,11 @@ exports.manga_update = function(req,res,next) {
 
 //delete item
 exports.manga_delete = function(req,res,next) {
+    let mangaID = req.params._id;
     Item.findOneAndDelete()
 };
 
 exports.manga_replace = function(req,res,next) {
-//replace item
-    //delete item
-    //create new one
+    let mangaID = req.params._id;
     Item.findOneAndReplace()
 };
