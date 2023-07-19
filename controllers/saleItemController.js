bc10878e360fe4f3eebe6e63e68fca9835714475
jrush <26492142+JacobMRush@users.noreply.github.com>
2023-07-19 15:22:16 -0700

    Modifying some of the routes, a bug appeared as a result of ending one of my routes in a variable path in express, resulting in the assumption that all (intended) routes are some invalid variable path

--- controllers/saleItemController.js
+++ controllers/saleItemController.js
@@ -1,112 +1,144 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Item = require("../models/saleItem");
 
 //get all items in the list sorted by publisher
-exports.manga_list = async function(req,res,next) { //used
-    try {
-        const docs = await Item.find({item_name: {$ne: null}, item_publisher: {$ne: null}}).sort({item_publisher: 1}).exec();
-        res.render('manga', {manga_list: docs});
-    } catch(err) {
-        console.log(err);
-    }
-}
+exports.manga_list = async function (req, res, next) {
+  //used
+  try {
+    const docs = await Item.find({
+      item_name: { $ne: null },
+      item_publisher: { $ne: null },
+    })
+      .sort({ item_publisher: 1 })
+      .exec();
+    res.render("manga", { manga_list: docs });
+  } catch (err) {
+    console.log(err);
+  }
+};
 //get a single item's details
-exports.manga_details = async function(req,res,next) { //used
-    let mangaID = req.params.id;
-    console.log(mangaID);
-    try {
-        const doc = await Item.findById(mangaID).exec();
-        res.render("viewItem", {doc: doc});
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
+exports.get_manga_categories = async function (req, res, next) {
+  try {
+    //querydb for all categories accross all documents
+    res.render("viewAllCategories");
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
+exports.manga_details = async function (req, res, next) {
+  //used
+  let mangaID = req.params.id;
+  console.log(mangaID);
+  try {
+    const doc = await Item.findById(mangaID).exec();
+    res.render("viewItem", { doc: doc });
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
 };
 //get items based on category
-exports.manga_category = async function(req,res,next) {  //used
-    let selectedCategory = req.params.category;
-    try {
-        const docs = await Item.find({item_categories: {$elemMatch: {category: selectedCategory}}}).exec();
-        res.render("viewCategory", {category_list: docs});
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
-}
+exports.manga_category = async function (req, res, next) {
+  //used
+  let selectedCategory = req.params.category;
+  try {
+    const docs = await Item.find({
+      item_categories: { $elemMatch: { category: selectedCategory } },
+    }).exec();
+    res.render("viewCategory", { category_list: docs });
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
 //get create item page
-exports.get_manga_create = async function (req,res, next) {
-    try {
-        await res.render('createItem');
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
-}
+exports.get_manga_create = async function (req, res, next) {
+  try {
+    await res.render("createItem");
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
 
 //create item
-exports.manga_create = async function(req,res,next) { //used
-    //create a new item via request body details?
-    try {
-        let itemdetails = req.body; //probably should verify that these are non-empty 
-        let citem = new Item(itemdetails);
-        await citem.save();
-        res.render('manga');
-    } catch(err) {
-        res.send("Error creating item");
-        console.log(err);
-    }
+exports.manga_create = async function (req, res, next) {
+  //used
+  //create a new item via request body details?
+  try {
+    let itemdetails = req.body; //probably should verify that these are non-empty
+    let citem = new Item(itemdetails);
+    await citem.save();
+    res.render("manga");
+  } catch (err) {
+    res.send("Error creating item");
+    console.log(err);
+  }
 };
 //update item
-exports.get_manga_update = async function(req,res,next) {
-    try {
-        res.render('updateItem');
-    }catch (err) {
-        res.render('404');
-        console.log(err);
-    }
-}
-exports.manga_update = async function(req,res,next) {
-    let mangaID = req.params.id;
-    try {
-        const doc = await Item.findOneAndUpdate();
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
+exports.select_manga_update = async function (req, res, next) {
+  try {
+    //get all manga postings
+    const docs = await Item.find({
+      item_name: { $ne: null },
+      item_publisher: { $ne: null },
+    }).exec();
+    res.render("selectUpdate", { manga_list: docs });
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
+exports.get_manga_update = async function (req, res, next) {
+  try {
+    res.render("updateItem");
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
+exports.manga_update = async function (req, res, next) {
+  let mangaID = req.params.id;
+  try {
+    const doc = await Item.findOneAndUpdate();
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
 
-    //find item to update
-    //change and update fields that are sent to us
-    //create field/object if it does not already exist
+  //find item to update
+  //change and update fields that are sent to us
+  //create field/object if it does not already exist
 };
 
 //delete item
-exports.manga_delete = async function(req,res,next) {
-    let mangaID = req.params.id;
-    try {
-        const doc = await Item.findOneAndDelete()
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
+exports.manga_delete = async function (req, res, next) {
+  let mangaID = req.params.id;
+  try {
+    const doc = await Item.findOneAndDelete();
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
 };
 
 //get manga replace
-exports.get_manga_replace = async function(req,res,next) {
-    try {
-        res.render()
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
-}
+exports.get_manga_replace = async function (req, res, next) {
+  try {
+    res.render();
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
+};
 
-exports.manga_replace = async function(req,res,next) {
-    let mangaID = req.params.id;
-    try {
-        const doc = await Item.findOneAndReplace();
-
-    } catch(err) {
-        res.render('404');
-        console.log(err);
-    }
+exports.manga_replace = async function (req, res, next) {
+  let mangaID = req.params.id;
+  try {
+    const doc = await Item.findOneAndReplace();
+  } catch (err) {
+    res.render("404");
+    console.log(err);
+  }
 };
--- routes/manga.js
+++ routes/manga.js
@@ -1,30 +1,29 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
 
-const itemController = require('../controllers/saleItemController');
+const itemController = require("../controllers/saleItemController");
 
 //home page will display all the manga
-router.get('/', itemController.manga_list);
+router.get("/", itemController.manga_list);
 // route /manga/:id will display a single posting's information
-router.get('/:id', itemController.manga_details);
-
+router.get("/item/:id", itemController.manga_details);
 
 //UPDATE POSTINGS
-router.post('/:id/update', itemController.manga_update);
-router.get('/:id/update', function(req,res,next) {
-    res.render('updateItem');
-});
+router.post("/update/:id", itemController.manga_update);
+router.get("/update/:id", itemController.get_manga_update);
+router.get("/update", itemController.select_manga_update); //user will selected an item and it will direct them to "/update/:id"
 
 //get all manga within a certain category
-router.get('/category/:category', itemController.manga_category);
+router.get("/categories", itemController.get_manga_categories);
+router.get("/category/:category", itemController.manga_category);
 
 //render and post create item
-router.get('/create', itemController.get_manga_create);
-router.post('/create', itemController.manga_create);
+router.get("/create", itemController.get_manga_create);
+router.post("/create", itemController.manga_create);
 
-router.post('/:id/delete', itemController.manga_delete);
+router.post("/:id/delete", itemController.manga_delete);
 
-router.get('/:id/replace', itemController.get_manga_replace);
-router.post('/:id/replace', itemController.manga_replace);
+router.get("/:id/replace", itemController.get_manga_replace);
+router.post("/:id/replace", itemController.manga_replace);
 
 module.exports = router;
